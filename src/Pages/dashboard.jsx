de99925dbb37bc1a902bacdcb9e3d5b4c47c5322
Korbanzo <Brooks.M.Korbin@gmail.com>
2026-03-02 19:05:52 -0600

    Setting up plant id

--- src/Pages/dashboard.jsx
+++ src/Pages/dashboard.jsx
@@ -1,15 +1,32 @@
 const Dashboard = () => {
+    const plantpicStyle = {
+        display: 'none'
+    };
 
     return (
         <>
             <div>
                 <h1>Dashboard</h1>
-                <input type="file" capture="environment" accept="image/*"/>
+                <label htmlFor="plantpic">Plant Picture</label>
+                <input id="plantpic" type="file" capture="environment" accept="image/*" style={plantpicStyle} onChange={(e) => identifyPlant(e.target.files[0])}/>
             </div>
-        
         </>
-    )
+    );
 
 }
+
+const identifyPlant = async (image) => {
+    const formData = new FormData();
+    formData.append('images', image);
+    formData.append('organs', 'auto');
+    
+    const response = await fetch(
+        `https://api.plant.id/v2/identify?api_key=${import.meta.env.VITE_PLANTNET_KEY}`,
+        { method: 'POST', body: formData }
+    );
+    
+    console.log(response.json());
+    return response.json();
+};
 
 export default Dashboard;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,26 @@
-import { Link } from "react-router-dom" 
+import { Link } from "react-router-dom"
 
 const Navbar = ({ style }) => {
-    const buttonStyle = {
-        border: "none",
-        borderRadius: "5px",
-        background: "#52b788",
-        color: "#F7F9F9"
+    const btnStyle = {
+        background: "#F7F9F9",
+        border: 'none',
+        borderRadius: '5px'
     };
 
     return (
         <>
             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', gap: '10px'}}>
                 <Link to="/">
-                    <button style={buttonStyle}>Home</button>
+                    <button style={btnStyle}>Home</button>
                 </Link>
                 <Link to="/signin">
-                    <button style={buttonStyle}>Sign In</button>
+                    <button style={btnStyle}>Sign In</button>
                 </Link>
                 <Link to="/signup">
-                    <button style={buttonStyle}>Sign Up</button>
+                    <button style={btnStyle}>Sign Up</button>
                 </Link>
                 <Link to="/dashboard">
-                    <button style={buttonStyle}>Dashboard *TDSO*</button>
+                    <button style={btnStyle}>Dashboard *TDSO*</button>
                 </Link>
             </div>
         </>
